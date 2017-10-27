5ef60f7a01c88c0323b6f7ca0324cbed445ca186
dhennessy86 <damienhennessy@yahoo.ie>
2017-10-27 11:32:16 +0100

    Update bot.py

--- bot.py
+++ bot.py
@@ -4,62 +4,52 @@ import random
 import csv
 import os, sys
 import time
+import requests
+import json
 
 #start time from OS
 starttime=time.time()
-
-User_Tweet_id = 0
-User_Tweet = ''
-Notify = False
 
 #Authorizes the code
 auth = ty.OAuthHandler(CONSUMER_KEY, CONSUMER_SECRET)
 auth.set_access_token(ACCESS_TOKEN, ACCESS_SECRET)
 api = ty.API(auth)
 
-#Set Users
-screen_name = ['<enter screen name>' , '<enter screen name>']
+#Set Users , default value of tweet id's
+screen_name = [["<enter user>",'1'] , ["<enter user>",'2']]
 
 #Create User output CSV files
-for user in screen_name:
-	f = open('%s_Tweets.csv' % user, 'wb')
+i = 0 
+while i < len(screen_name):
+	f = open('%s_Tweets.csv' % screen_name[i][0], 'wb')
 	writer = csv.writer(f)
 	writer.writerow(["Tweet", "Created_at" , "Tweet ID"])
-
+	i += 1
+	
 def getUsersTweet(api):
 	global message
-	global User_Tweet_id
-	global User_Tweet
-	for user in screen_name:  							
-		r = api.user_timeline(user , count = 1)  		# count sets the no of tweets to rx
-		i = 0
-		while i < len(r):
-			User_Tweets = r[i]
-			User_Tweet = User_Tweets.text.encode("utf-8")
-			User_Tweet_id =  "[" + str(User_Tweets.id) + "]"
-			message = ("Username: " + str(user) + "\nTweet: " + str(User_Tweet))
-			if Notify == True:
-				print "printing"
-				#Output tweets to CSV file 
-				f = open('%s_Tweets.csv' % user, 'a+')
-				writer = csv.writer(f)
-				writer.writerow([User_Tweet, User_Tweets.created_at , User_Tweet_id ])
-			i += 1
+	i = 0 
+	while i < len(screen_name):
+		old_id = screen_name[i][1]										# Loop through users, terminal and into uniq csv
+		user = screen_name[i][0]
+		r = api.user_timeline(user , count = 1 ,tweet_mode='extended')  # count sets the no of tweets to rx
+		User_Tweets = r[0]
+		User_Tweet = User_Tweets.full_text.encode("utf-8")
+		screen_name[i][1] =  "[" + str(User_Tweets.id) + "]"
+		message = ("Username: " + str(user) + "\nTweet: " + str(User_Tweet))
+		if screen_name[i][1] == old_id:									# Loop through tweet id untill user sends new tweet
+			print "No new Tweets, sleep +4 min"							# if id's are same pass
+			pass
+		else:															# new id means new tweet, post tweet
+			print message
+			#Output tweets to CSV file 
+			f = open('%s_Tweets.csv' % user, 'a+')
+			writer = csv.writer(f)
+			writer.writerow([User_Tweet, User_Tweets.created_at , screen_name[i][1] ])
+		i += 1
 
 while True: 
 	#Main Driver Script
 	if __name__ == "__main__":
-		old_id = User_Tweet_id
 		getUsersTweet(api)
-		time.sleep(6.0 - ((time.time() - starttime) % 6.0))	# Sets the time for the script to sleep , change both numbers , 5 = 5secs
-		if User_Tweet_id == old_id:				# Loop through tweet id untill user sends new tweet
-			print "No new Tweets +6 sec"			# if id's are same pass
-			Notify = False
-			pass
-		else:							# new id means new tweet, post tweet
-			print message
-			Notify = True
-
-			
-			
-
+		time.sleep(240.0 - ((time.time() - starttime) % 240cal.0))				# Sets the time for the script to sleep , change both numbers , 5 = 5secs
